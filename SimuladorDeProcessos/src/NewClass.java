a5d082dd047c5ec7e9804adcc978ba2c38a34c14
sergioramossilva <sergiormsilva@yahoo.com>
2015-04-30 20:06:23 -0300

    travis

--- SimuladorDeProcessos/src/NewClass.java
+++ SimuladorDeProcessos/src/NewClass.java
@@ -22,5 +22,9 @@ public class NewClass {
     public static void main(String[] args) {
         
         System.out.println("Hello Word!");
+        System.out.println("Hello Word!");
+        System.out.println("Hello Word!");
+        System.out.println("Hello Word!");
+        System.out.println("Hello Word!");
     }
 }
